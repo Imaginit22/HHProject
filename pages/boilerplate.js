26a3fb40c44a74313482f72166053666e544fbe9
Enrj-Drunk <99850420+Imaginit22@users.noreply.github.com>
2024-11-23 13:13:45 -0500

    Update boilerplate.js

--- pages/boilerplate.js
+++ pages/boilerplate.js
@@ -1,5 +1,27 @@
 import Szhead from '../components/Szhead';
-const BoilerplatePage = () => {
+const BoilerplatePage = () => {   
+    //global variables are called hooks, useState for good things.
+    const [hook, setHook] = useState([]); 
+
+    //how to make a function. async allows functions that normally
+    //run asynchronously to be waited for, using await.
+    const func = (async (param) => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({'whee': param})
+        }
+        await fetch('/endpoint', options)
+        print(yaya)
+    })
+
+    //code goes here for on-run things
+    useEffect(() => {
+        console.log("This is print in usefeccted")
+    }, []);
+
     return(
         <div>
             <Szhead title={'about'}/>
