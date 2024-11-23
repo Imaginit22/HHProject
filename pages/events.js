40787dac7880e485de65e6897f8f11ec96b1b2da
Thomas Brittain <thomas.brittain3@gmail.com>
2024-11-23 18:18:26 -0500

    hi

--- pages/events.js
+++ pages/events.js
@@ -8,7 +8,7 @@ const events = () => {
     //run asynchronously to be waited for, using await.
 
     //code goes here for on-run things
-    /*useEffect(() => {
+    useEffect(() => {
         (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
         ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"})
         async function initMap() {
@@ -119,25 +119,15 @@ const events = () => {
           ];
           
           initMap();
-    }, []);*/
+    }, []);
 
     //html goes here
     return(
         <div>
             <Head title={'about'}/>
-            <html>
-                <head>
-                    <title>Advanced Markers with HTML</title>
-
-                    <script src="https://use.fontawesome.com/releases/v6.2.0/js/all.js"></script>
-
-                    <link rel="stylesheet" type="text/css" href="./styles.css" />
-                    <script type="module" src="./GM.js"></script>
-                </head>
-                <body>
-                    <div id="map"></div>
-                </body>
-            </html>
+            <body>
+            <p align="center"><iframe src ="GMTesting.html" width="1000" height="750"></iframe></p>
+            </body>
         </div>
     );
 }
