3b361f034f5e9674acf6df05265f13f57fa0ef63
ZN-DVSS <zeyadmnasr07@gmail.com>
2024-11-23 18:00:19 -0500

    1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import Head from '../components/Head'; 
 
-
-
 const HomePage = () => {
     useEffect(() => {
         const imgLink = document.getElementById("imgLink");
@@ -18,19 +16,9 @@ const HomePage = () => {
             <Head title="Home" />
             <header>
                 <div className="top">
-                    <h1 className="centerText">NDM WHEEE</h1>   
+                    <h1 className="centerText">NDM WHEEE</h1>
                 </div>
             </header>
-
-            <body style="background-color: powderblue;">
-        
-                <h1> Name Doesn't Matter😁</h1>
-                <h2> The first interactive map of all events in the Hamilton Region.</h2>
-                <p> Create an account to share your events, or scroll down to explore available events.</p>
-                
-                <p align="center"><iframe src ="thomas.html" width="1000" height="750"></iframe></p>
-       
-    </body>
         </div>
         
     );
